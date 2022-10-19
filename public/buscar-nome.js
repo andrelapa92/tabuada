f3cdd18c6670623aa398ab33dace76c5b804b72b
andrelapa92 <andrelapa92@gmail.com>
2022-10-19 18:01:03 -0300

    teste

--- public/buscar-nome.js
+++ public/buscar-nome.js
@@ -4,10 +4,12 @@ console.log(linhas);
 function destacaTexto() {
     let inputTexto = document.getElementById("inputtext").value;
     for (let i = 0; i <= linhas.length; i++) {
-        console.log(linhas[i].innerHTML);
+        console.log(linhas[i].innerHTML.includes(inputTexto));
         console.log(inputTexto);
-        if (linhas[i].innerHTML.includes(inputTexto)) {
+        /*
+        if (linhas[i].innerHTML.includes(inputTexto) == true) {
             console.log(linhas[i].innerHTML);
         }
+        */
     }
 }
